15c8a58198588c3b370e32b33cfd2f35a1a66980
Aman Saini <aman.singh756888@gmail.com>
2023-04-02 21:29:59 +0530

    some practice questions

---
+++ Assignments/BobPainter.java
@@ -0,0 +1,35 @@
+package Assignments;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class BobPainter {
+    public static int getBucketCount(double width, double height, double areaPerbucket,int noOfBucketsAtHome)  {
+        if(width <= 0 || height <=0 || areaPerbucket<=0 || noOfBucketsAtHome <0){
+            return -1;
+        }
+        return ((int)((width * height)/areaPerbucket) - noOfBucketsAtHome);
+
+    }
+    public static int getBucketCount(double width, double height, double areaPerbucket)  {
+        if(width <= 0 || height <=0 || areaPerbucket<=0 ){
+            return -1;
+        }
+        return ((int)((width * height)/areaPerbucket));
+
+    }
+    public static int getBucketCount(double area, double areaPerbucket)  {
+        if(area <=0 || areaPerbucket<=0 ){
+            return -1;
+        }
+        return (int)(area/areaPerbucket);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getBucketCount(100, 100, 200, 2));
+        System.out.println(getBucketCount(0, 100, 200, 2));
+        System.out.println(getBucketCount(20, 25, 10));
+    }
+
+    
+}
